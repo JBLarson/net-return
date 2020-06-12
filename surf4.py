a79954ef76950bd05c686fdf9f442e1f00cf5761
Above the Book <58607747+AbovetheBook@users.noreply.github.com>
2020-06-12 14:37:28 -0400

    create 4 surface plots displaying net-return

--- surf4.py
+++ surf4.py
@@ -13,62 +13,57 @@ ax2 = fig.add_subplot(222, projection='3d')
 ax3 = fig.add_subplot(223, projection='3d')
 ax4 = fig.add_subplot(224, projection='3d')
 
-#3b create and assign x/y/X, Y/Z variables
+#3b create x/y variables, with values determined by 'range variables' from nr_input 
+X, Y/Z variables
 x = np.arange(inp.exp_spr-6, inp.exp_spr+6, 1)
 y = np.arange(inp.lo_tp, inp.hi_tp, 1)
+
+#3c use 'x' and 'y' to create Numpy meshgrid 'X, Y', assign net_map (from nr_results) to 'Z'
 X, Y = np.meshgrid(x, y)
 Z = nr.net_map
 
-#3c create colormap, (for each axis:) define subplot titles and create surface plot
+#3d create colormap. For each axis: define subplot titles and create surface plot
 mycmap = plt.get_cmap('RdYlGn')
-
 
 ax1.set_title('Spread View')
 ax1.set_zlabel('Result ($)')
 ax1.set_xlabel('Spread')
 ax1.set_ylabel('Total Points')
-
 surf1 = ax1.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=mycmap)
 ax1.set_zlim(np.min(Z), np.max(Z))
 fig.colorbar(surf1, ax=ax1, shrink=.7, aspect=15)
-
 
 ax2.set_title('Total Point Display')
 ax2.set_zlabel('Result ($)')
 ax2.set_xlabel('Spread')
 ax2.set_ylabel('Total Points')
-
 surf2 = ax2.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=mycmap)
 ax2.set_zlim(np.min(Z), np.max(Z))
 fig.colorbar(surf2, ax=ax2, shrink=.7, aspect=15)
-
 
 ax3.set_title('Spread')
 ax3.set_xlabel('Spread')
 ax3.set_zlabel('Result ($)')
 ax3.set_yticks([])
-
 surf3 = ax3.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=mycmap)
 ax3.set_zlim(np.min(Z), np.max(Z))
 fig.colorbar(surf3, ax=ax3, shrink=.7, aspect=15)
-
 
 ax4.set_title('Total Pt')
 ax4.set_zlabel('Result ($)')
 ax4.set_ylabel('Total Points')
 ax4.set_xticks([])
-
 surf4 = ax4.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=mycmap)
 ax4.set_zlim(np.min(Z), np.max(Z))
 fig.colorbar(surf4, ax=ax4, shrink=.7, aspect=15)
 
 
-#3d assign view angles to each surface plot (elevation, azimuth)
+#3e assign view angles to each surface plot (elevation, azimuth)
 ax1.view_init(40, 55)
 ax2.view_init(40, 35)
 ax3.view_init(0, 90)
 ax4.view_init(0, 180)
 
 
-#3e display matplotlib figure
+#3f display matplotlib figure
 plt.show()
