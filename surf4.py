5fff54c4a99efcadd4d1ddb23a7c9afa5183e3eb
Above the Book <58607747+AbovetheBook@users.noreply.github.com>
2020-06-12 13:53:12 -0400

    create 4 surface plots displaying net-return

--- surf4.py
+++ surf4.py
@@ -2,10 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import axes3d
 
+#3a import nr_results & nr_input, create matplotlib figure, add 4 subplots displayed 2x2 
 import nr_results as nr
-import input_nr as inp
-
-
+import nr_input as inp
 
 fig = plt.figure(figsize=(10,6))
 fig.suptitle('Above the Book - NetRet Model')
@@ -14,11 +13,14 @@ ax2 = fig.add_subplot(222, projection='3d')
 ax3 = fig.add_subplot(223, projection='3d')
 ax4 = fig.add_subplot(224, projection='3d')
 
-
+#3b create and assign x/y/X, Y/Z variables
 x = np.arange(inp.exp_spr-6, inp.exp_spr+6, 1)
 y = np.arange(inp.lo_tp, inp.hi_tp, 1)
 X, Y = np.meshgrid(x, y)
 Z = nr.net_map
+
+#3c create colormap, (for each axis:) define subplot titles and create surface plot
+mycmap = plt.get_cmap('RdYlGn')
 
 
 ax1.set_title('Spread View')
@@ -26,14 +28,9 @@ ax1.set_zlabel('Result ($)')
 ax1.set_xlabel('Spread')
 ax1.set_ylabel('Total Points')
 
-
-
-mycmap = plt.get_cmap('RdYlGn')
 surf1 = ax1.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=mycmap)
 ax1.set_zlim(np.min(Z), np.max(Z))
 fig.colorbar(surf1, ax=ax1, shrink=.7, aspect=15)
-
-
 
 
 ax2.set_title('Total Point Display')
@@ -47,24 +44,18 @@ fig.colorbar(surf2, ax=ax2, shrink=.7, aspect=15)
 
 
 ax3.set_title('Spread')
-ax3.set_yticks([])
 ax3.set_xlabel('Spread')
 ax3.set_zlabel('Result ($)')
+ax3.set_yticks([])
 
-
-mycmap = plt.get_cmap('RdYlGn')
 surf3 = ax3.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=mycmap)
 ax3.set_zlim(np.min(Z), np.max(Z))
 fig.colorbar(surf3, ax=ax3, shrink=.7, aspect=15)
 
 
-
-
 ax4.set_title('Total Pt')
 ax4.set_zlabel('Result ($)')
 ax4.set_ylabel('Total Points')
-
-#ax4.set_axis_off()
 ax4.set_xticks([])
 
 surf4 = ax4.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=mycmap)
@@ -72,11 +63,12 @@ ax4.set_zlim(np.min(Z), np.max(Z))
 fig.colorbar(surf4, ax=ax4, shrink=.7, aspect=15)
 
 
+#3d assign view angles to each surface plot (elevation, azimuth)
 ax1.view_init(40, 55)
 ax2.view_init(40, 35)
 ax3.view_init(0, 90)
-ax4.view_init(0, 0)
+ax4.view_init(0, 180)
 
 
-
+#3e display matplotlib figure
 plt.show()
