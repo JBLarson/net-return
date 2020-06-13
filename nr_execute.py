5d512d060b2e17795c8299adbb4309e37e4b28b5
Above the Book <58607747+AbovetheBook@users.noreply.github.com>
2020-06-13 13:31:45 -0400

    execute net-result model

--- nr_execute.py
+++ nr_execute.py
@@ -4,6 +4,13 @@ import pandas as pd
 #import scenario and matrix DataFrames / surface-plot function
 from nr_input import tdf, scndf
 from nr_results import nrvisual
+import nr_input as inp
+
+#localize variables from inp
+o1s, u1s, o1r, u1r, o1w, u1w = inp.o1s, inp.u1s, inp.o1r, inp.u1r, inp.o1w, inp.u1w
+f1s, d1s, f1r, d1r, f1w, d1w = inp.f1s, inp.d1s, inp.f1r, inp.d1r, inp.f1w, inp.d1w
+
+
 
 #define scenario stats function
 def scn_stats():
@@ -21,5 +28,18 @@ def surf_decr(surf4):
 @surf_decr
 def surf4():
 	nrvisual()
+
+#desc = False
+desc = True
+if desc == True:
+
+	print("Over:",o1s,"pts - Risk: $",o1r,"Net: $",round(o1w, ndigits=2))
+	print("Under:",u1s,"pts - Risk: $",u1r,"Net: $",round(u1w, ndigits=2))
+	print()
+	print("Fav:",f1s,"pt - Risk: $",f1r,"Net: $",round(f1w, ndigits=2))
+	print("Dog:",d1s,"pt - Risk: $",d1r,"Net: $",round(d1w, ndigits=2))
+	print()
+
+
 
 surf4()
