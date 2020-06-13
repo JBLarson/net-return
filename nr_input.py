be9c6cc48136ae3191b815ad2be553c99e313bad
Above the Book <58607747+AbovetheBook@users.noreply.github.com>
2020-06-13 13:21:13 -0400

    risk / odds / score for 4 wagers

--- nr_input.py
+++ nr_input.py
@@ -1,3 +1,6 @@
+from pandas import DataFrame
+import pandas as pd
+
 #1a create lambda to find win
 g = lambda r, o : r * (o-1)
 
@@ -7,7 +10,7 @@ u1s, u1o, u1r = float(44), float(1.7), float(50)
 f1s, f1o, f1r = float(-3), float(1.67), float(60)
 d1s, d1o, d1r = float(5), float(1.87), float(50)
 
-#1c variables defining pts covered by visual
+#1c create pt ranges for stats and visual
 exp_spr = -4
 spr_pts = range(exp_spr-6, exp_spr+6)
 avg_tp = (o1s + u1s)/2
@@ -18,14 +21,29 @@ tp_pts = range(lo_tp, hi_tp)
 #1d tp/spr win variables
 o1w, u1w, f1w, d1w = g(o1r, o1o), g(u1r,u1o), g(f1r,f1o), g(d1r,d1o)
 
-#1e describe bets (toggle on/off with variable)
-desc = False
-#desc = True
-if desc == True:
 
-	print("Over:",o1s,"pts - Risks: $",o1r,"Net: $",round(o1w, ndigits=2))
-	print("Under:",u1s,"pts - Risks: $",u1r,"Net: $",round(u1w, ndigits=2))
-	print()
-	print("Fav:",f1s,"pt - Risks: $",f1r,"Net: $",round(f1w, ndigits=2))
-	print("Dog:",d1s,"pt - Risks: $",d1r,"Net: $",round(d1w, ndigits=2))
-	print()
+#1e create (t)p and (s)pr variables, create 4-way scenario variables (over/under/fav/dog)
+tww, twL, tLw = o1w + u1w, o1w - u1r, u1w - o1r
+sww, swL, sLw = f1w + d1w, f1w - d1r, d1w - f1r
+
+wLwL, wLww, wLLw = twL + swL, twL + sww, twL + sLw
+wwwL, wwww, wwLw = tww + swL, tww + sww, tww + sLw #all tp win-win values
+LwwL, Lwww, LwLw = tLw + swL, tLw + sww, tLw + sLw
+
+scenarios = {1: wLwL, 2: wLww, 3: wLLw, 4: wwwL, 5: wwww, 6: wwLw, 7: LwwL, 8: Lwww, 9: LwLw}
+scn_list = [scenarios[scn] for scn in scenarios]
+
+#tp / spr scenario dictionaries & lists
+scntp = [swL, sww, sLw, swL, sww, sLw, swL, sww, sLw]
+scnspr = [twL, twL, twL, tww, tww, tww, tLw, tLw, tLw]
+
+sscwL, sscww, sscLw = [wLwL, wwwL, LwwL], [wLww, wwww, Lwww], [wLLw, wwLw, LwLw]
+tscwL, tscww, tscLw = [wLwL, wLww, wLLw], [wwwL, wwww, wwLw], [LwwL, Lwww, LwLw]
+
+# <DataFrame> scenario matrix w/ tpscn as columns
+tdf = pd.DataFrame({'wLtp': tscwL, 'wwtp': tscww, 'Lwtp': tscLw}, index = ['wLspr', 'wwspr', 'Lwspr'])
+
+# <DataFrame> scenario sum and diff
+key = ['wLwL', 'wwwL', 'LwwL', 'wLww', 'wwww', 'Lwww', 'wLLw', 'wwLw', 'LwLw']
+scndf = pd.DataFrame({'key': key, 'scn($)': scn_list, 'tp($)': scntp, 'spr($)': scnspr})
+
