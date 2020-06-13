d317661ce3dba6a66dc716458462a90a50452cf7
Above the Book <58607747+AbovetheBook@users.noreply.github.com>
2020-06-13 13:25:18 -0400

    risk / odds / score for 4 wagers

--- nr_input.py
+++ nr_input.py
@@ -1,16 +1,16 @@
 from pandas import DataFrame
 import pandas as pd
 
-#1a create lambda to find win
+#create lambda to find win
 g = lambda r, o : r * (o-1)
 
-#1b tp/spr input variables
+#tp/spr input variables
 o1s, o1o, o1r = float(40), float(1.9), float(40)
 u1s, u1o, u1r = float(44), float(1.7), float(50)
 f1s, f1o, f1r = float(-3), float(1.67), float(60)
 d1s, d1o, d1r = float(5), float(1.87), float(50)
 
-#1c create pt ranges for stats and visual
+#create pt ranges for stats and visual
 exp_spr = -4
 spr_pts = range(exp_spr-6, exp_spr+6)
 avg_tp = (o1s + u1s)/2
@@ -18,11 +18,11 @@ lo_tp = int(avg_tp - 6)
 hi_tp = int(avg_tp + 6)
 tp_pts = range(lo_tp, hi_tp)
  
-#1d tp/spr win variables
+#tp/spr win variables
 o1w, u1w, f1w, d1w = g(o1r, o1o), g(u1r,u1o), g(f1r,f1o), g(d1r,d1o)
 
 
-#1e create (t)p and (s)pr variables, create 4-way scenario variables (over/under/fav/dog)
+#create (t)p and (s)pr variables, create 4-way scenario variables (over/under/fav/dog)
 tww, twL, tLw = o1w + u1w, o1w - u1r, u1w - o1r
 sww, swL, sLw = f1w + d1w, f1w - d1r, d1w - f1r
 
