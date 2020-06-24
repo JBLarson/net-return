aea75dcac14cb600b89c95784d64c7e5fe268a46
Above the Book <58607747+AbovetheBook@users.noreply.github.com>
2020-06-24 12:57:43 -0400

    surface/scatter plot visuals for net-return

--- nr_ss.py
+++ nr_ss.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import axes3d
 
-#import input_nr, create tp/spr DataFrames, add ranges to each
+#import nr_input, create tp/spr DataFrames, add ranges to each
 import nr_input as inp
 
 tp_df = DataFrame()
@@ -32,7 +32,6 @@ for pt in inp.spr_pts:
 			d1_rez.append(inp.d1w)
 
 def nr_result():
-
 	#append lists to df's and calculate net results for each
 	tp_df['o1'] = o1_rez
 	tp_df['u1'] = u1_rez
@@ -52,14 +51,13 @@ def nr_result():
 
 	#create 12x12 Numpy array of net-results, use that to create net-return array
 
-
 	netd0, netd1, netd2, netd3, netd4, netd5 = [], [], [], [], [], []
 	netd6, netd7, netd8, netd9, netd10, netd11= [], [], [], [], [], []
 	for n in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11']:
-		for s in [f'sum(spr_net[11]+tp_net[{n}])', f'sum(spr_net[10]+tp_net[{n}])', f'sum(spr_net[9]+tp_net[{n}])', f'sum(spr_net[8]+tp_net[{n}])', f'sum(spr_net[7]+tp_net[{n}])', f'sum(spr_net[6]+tp_net[{n}])', f'sum(spr_net[5]+tp_net[{n}])', f'sum(spr_net[4]+tp_net[{n}])', f'sum(spr_net[3]+tp_net[{n}])', f'sum(spr_net[2]+tp_net[{n}])', f'sum(spr_net[1]+tp_net[{n}])', f'sum(spr_net[0]+tp_net[{n}])']:
+		for s in [f'sum(tp_net[11]+spr_net[{n}])', f'sum(tp_net[10]+spr_net[{n}])', f'sum(tp_net[9]+spr_net[{n}])', f'sum(tp_net[8]+spr_net[{n}])', f'sum(tp_net[7]+spr_net[{n}])', f'sum(tp_net[6]+spr_net[{n}])', f'sum(tp_net[5]+spr_net[{n}])', f'sum(tp_net[4]+spr_net[{n}])', f'sum(tp_net[3]+spr_net[{n}])', f'sum(tp_net[2]+spr_net[{n}])', f'sum(tp_net[1]+spr_net[{n}])', f'sum(tp_net[0]+spr_net[{n}])']:
 			eval(f'netd{n}').append(eval(s))
 
-	global net_map
+	global net_map, net_ret, X, Y, Z, Z1L
 
 	net_map = np.array([netd0, netd1, netd2, netd3, netd4, netd5, netd6, netd7, netd8, netd9, netd10, netd11])
 
@@ -69,27 +67,7 @@ def nr_result():
 	for n in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11']:
 		for s in [f'netd{n}[0]/trisk', f'netd{n}[1]/trisk', f'netd{n}[2]/trisk', f'netd{n}[3]/trisk', f'netd{n}[4]/trisk', f'netd{n}[5]/trisk', f'netd{n}[6]/trisk', f'netd{n}[7]/trisk', f'netd{n}[8]/trisk', f'netd{n}[9]/trisk', f'netd{n}[10]/trisk', f'netd{n}[11]/trisk']:
 			eval(f'net{n}').append(eval(s))
-	global net_ret
 	net_ret = np.array([net0, net1, net2, net3, net4, net5, net6, net7, net8, net9, net10, net11])
-
-
-#create decorator for visual / net-map
-def roi_decr(roi_visual):
-	def wrapper():
-		nr_result()
-		roi_visual()
-		print()
-	return wrapper
-
-@roi_decr
-
-def roi_visual():
-
-	showroi = input('display roi figure: (y/n)')
-
-	fig = plt.figure(figsize=(10,6))
-	ax1 = fig.add_subplot(111, projection='3d')
-	#ax2 = fig.add_subplot(111, projection='3d')
 
 	#create x/y variables, with values determined by 'range variables' from nr_input 
 	x = np.arange(inp.exp_spr-6, inp.exp_spr+6, 1)
@@ -98,83 +76,168 @@ def roi_visual():
 	#use 'x' and 'y' to create Numpy meshgrid 'X, Y', assign net_map (from nr_results) to 'Z'
 	X, Y = np.meshgrid(x, y)
 	#change z1 to z and comment Z to display net-return graphic instead of net-result
-	
-	if showroi == 'y': 
-		Z, Z1L = net_ret, []
+	Z, Z1L = net_ret, []
 
-		for zz in Z:
-			for z in zz: Z1L.append(z)
-
-		#create colormap. For each axis: define subplot titles and create surface plot
-		mycmap = plt.get_cmap('RdYlGn')
-		
-		ax1.set_zlabel('\nROI', fontsize=16)
-		ax1.set_xlabel('\nSpread', fontsize=16)
-		ax1.set_ylabel('\nTotal Points', fontsize=16)
-		surf1 = ax1.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=mycmap)
-		ax1.set_zlim(np.min(Z), np.max(Z))
-
-		scatter1 = ax1.scatter(X, Y, Z, c = Z1L, cmap='RdYlGn', edgecolors='black')
-
-		#assign view angles to each surface plot (elevation, azimuth)
-		ax1.view_init(45, 45)
-
-		#display matplotlib figure
-		plt.show()
+	for zz in Z:
+		for z in zz: Z1L.append(z)
 
 
-#create decorator for visual / net-map
-def net_decr(net_visual):
+#decorator for surf-scat visual
+def ss_decr(surfscat):
 	def wrapper():
 		nr_result()
-		net_visual()
+		surfscat()
 		print()
 	return wrapper
 
-@net_decr
+@ss_decr
+def surfscat():
+	fig = plt.figure(figsize=(8,6))
+	ax1 = fig.add_subplot(211, projection='3d')
+	ax2 = fig.add_subplot(212, projection='3d')
+	mycmap = plt.get_cmap('RdYlGn')
+	
+	ax1.set_zlabel('\nROI(%)', fontsize=16)
+	ax1.set_xlabel('\nSpread', fontsize=16)
+	ax1.set_ylabel('\nTotal Points', fontsize=16)
+	surf1 = ax1.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=mycmap)
+	ax1.set_zlim(np.min(Z), np.max(Z))
 
-def net_visual():
+	ax2.set_zlabel('\nROI(%)', fontsize=16)
+	ax2.set_xlabel('\nSpread', fontsize=16)
+	ax2.set_ylabel('\nTotal Points', fontsize=16)
+	scatter1 = ax2.scatter(X, Y, Z, c = Z1L, cmap='RdYlGn', marker='D', edgecolors='black')
+	ax2.set_zlim(np.min(Z), np.max(Z))
+	
+	ax1.view_init(30, 45)
+	ax2.view_init(30, 45)
+	plt.show()
 
-	fig = plt.figure(figsize=(10,6))
+#decorator for surface plot visual
+def surf_decr(surf):
+	def wrapper():
+		nr_result()
+		surf()
+		print()
+	return wrapper
+
+@surf_decr
+def surf():
+	fig = plt.figure(figsize=(8,4))
 	ax1 = fig.add_subplot(111, projection='3d')
-	#ax2 = fig.add_subplot(111, projection='3d')
+	mycmap = plt.get_cmap('RdYlGn')
+	ax1.set_zlabel('\nROI(%)', fontsize=16)
+	ax1.set_xlabel('\nSpread', fontsize=16)
+	ax1.set_ylabel('\nTotal Points', fontsize=16)
+	surf1 = ax1.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=mycmap)
+	ax1.set_zlim(np.min(Z), np.max(Z))
+	ax1.view_init(30, 45)
+	plt.show()
 
-	#create x/y variables, with values determined by 'range variables' from nr_input 
+#decorator for scatter plot visual
+def scatter_decr(scatter):
+	def wrapper():
+		nr_result()
+		scatter()
+		print()
+	return wrapper
+
+@scatter_decr
+def scatter():
+	fig = plt.figure(figsize=(8,4))
+	ax1 = fig.add_subplot(111, projection='3d')
+	mycmap = plt.get_cmap('RdYlGn')
+	ax1.set_zlabel('\nROI(%)', fontsize=16)
+	ax1.set_xlabel('\nSpread', fontsize=16)
+	ax1.set_ylabel('\nTotal Points', fontsize=16)
+	scatter1 = ax1.scatter(X, Y, Z, c = Z1L, cmap='RdYlGn', marker='D', edgecolors='black')
+	ax1.set_zlim(np.min(Z), np.max(Z))
+	ax1.view_init(30, 45)
+	plt.show()
+
+
+
+
+
+#decorator for visual / net-map
+def ssroi_decr(ssroi_visual):
+	def wrapper():
+		nr_result()
+		ssroi_visual()
+	return wrapper
+
+@ssroi_decr
+
+def ssroi_visual():
+
+	fig = plt.figure(figsize=(8,4))
+	ax1 = fig.add_subplot(111, projection='3d')
 	x = np.arange(inp.exp_spr-6, inp.exp_spr+6, 1)
 	y = np.arange(inp.lo_tp, inp.hi_tp, 1)
-
-	#use 'x' and 'y' to create Numpy meshgrid 'X, Y', assign net_map (from nr_results) to 'Z'
 	X, Y = np.meshgrid(x, y)
-	#change z1 to z and comment Z to display net-return graphic instead of net-result	
+	Z, Z1L = net_ret, []
+	for zz in Z:
+		for z in zz: Z1L.append(z)
+	mycmap = plt.get_cmap('RdYlGn')
+	ax1.set_zlabel('\nROI', fontsize=16)
+	ax1.set_xlabel('\nSpread', fontsize=16)
+	ax1.set_ylabel('\nTotal Points', fontsize=16)
+	surf1 = ax1.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=mycmap)
+	ax1.set_zlim(np.min(Z), np.max(Z))
+	scatter1 = ax1.scatter(X, Y, Z, c = Z1L, cmap='RdYlGn', edgecolors='black')
+	ax1.view_init(55, 40)
 
-	shownet = input('display net figure: (y/n)')
-
-	if shownet == 'y':
-		Z, Z1L = net_map, []
-
-		for zz in Z:
-			for z in zz: Z1L.append(z)
-
-		#create colormap. For each axis: define subplot titles and create surface plot
-		mycmap = plt.get_cmap('RdYlGn')
-		
-		ax1.set_zlabel('\nNet($)', fontsize=16)
-		ax1.set_xlabel('\nSpread', fontsize=16)
-		ax1.set_ylabel('\nTotal Points', fontsize=16)
-		surf1 = ax1.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=mycmap)
-		ax1.set_zlim(np.min(Z), np.max(Z))
-
-		scatter1 = ax1.scatter(X, Y, Z, c = Z1L, cmap='RdYlGn', edgecolors='black')
-
-		#assign view angles to each surface plot (elevation, azimuth)
-		ax1.view_init(45, 45)
-
-		#display matplotlib figure
-		plt.show()
+	plt.show()
 
 
+#decorator for visual / net-map
+def ssnet_decr(ssnet_visual):
+	def wrapper():
+		nr_result()
+		ssnet_visual()
+	return wrapper
 
-roi_visual()
+@ssnet_decr
 
-net_visual()
+def ssnet_visual():
+	fig = plt.figure(figsize=(8,4))
+	ax1 = fig.add_subplot(111, projection='3d')
+	x = np.arange(inp.exp_spr-6, inp.exp_spr+6, 1)
+	y = np.arange(inp.lo_tp, inp.hi_tp, 1)
+	X, Y = np.meshgrid(x, y)
+	Z, Z1L = net_map, []
+	for zz in Z:
+		for z in zz: Z1L.append(z)
+	mycmap = plt.get_cmap('RdYlGn')
+	ax1.set_zlabel('\nNet($)', fontsize=16)
+	ax1.set_xlabel('\nSpread', fontsize=16)
+	ax1.set_ylabel('\nTotal Points', fontsize=16)
+	surf1 = ax1.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=mycmap)
+	ax1.set_zlim(np.min(Z), np.max(Z))
+	scatter1 = ax1.scatter(X, Y, Z, c = Z1L, cmap='RdYlGn', edgecolors='black')
+	ax1.view_init(35, 40)
+
+	plt.show()
+
+
+
+
+
+#options to display 3D visuals
+#surface plot, scatter plot, surface / scatter (seperate axis')
+#surface / scatter ROI, surface / scatter net (same axis)
+
+d_surf, d_scat, d_ss = ('n'), ('n'), ('y')
+d_ssroi, d_ssnet = ('n'), ('y')
+
+
+if d_surf == 'y': surf()
+if d_scat == 'y': scatter()
+if d_ss == 'y': surfscat()
+if d_ssroi == 'y': ssroi_visual()
+if d_ssnet == 'y': ssnet_visual()
+
+
+
+
 
